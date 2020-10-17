3b77930ff4b3968a6d932a44d95a0288dabe5bfa
Patrick Taylor <ptrcktylr@icloud.com>
2020-10-17 13:33:51 -0400

    addes pages resource to routes, finished pages controller, index page displays all images for specified user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,17 @@
 class PagesController < ApplicationController
 
-  def show
-    render template: "pages/home"
-  end
+  def index
+    flickr = Flickr.new
 
+    if params[:user_id]
+      # get public photos from user
+      begin
+        photos_info = flickr.people.getPublicPhotos(api_key: ENV["FLICKR_API_KEY"], user_id: params[:user_id], format: "rest")
+      rescue => exception
+        redirect_to root_path, alert: "Couldn't find user with the specified ID"
+      else
+        @photo_links = photos_info.map {|photo_info| Flickr.url_b(photo_info)}
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  root "pages#show"
+  root "pages#index"
+  resources :pages, only: [:index] 
 end
